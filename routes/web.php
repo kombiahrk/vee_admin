e65a5259e2573e450fe2f227fadc3fb31d0b6f7c
Kombiah <kombiahrk@yahoo.com>
2021-06-14 20:28:34 +0530

    home view correction

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('logout', 'App\Http\Controllers\Auth\LoginController@logout');
 
